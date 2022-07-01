e454fa0d7917b21593f29b5900bde42b96cbef4f
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-07-02 01:01:54 +0900

    video 120 is done

--- list.py
+++ list.py
@@ -353,16 +353,16 @@
 
 ### video 114 processing nested lists
 ### video 115 solution to nonspam challange
-menu = [
-    ["egg", "bacon"],
-    ["egg", "sausage", 'bacon'],
-    ["egg", "spam"],
-    ["egg", 'bacon', "spam"],
-    ["egg", 'bacon', 'sausage', 'spam'],
-    ["spam", 'bacon', 'sausage', 'spam'],
-    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
-    ["spam", "egg", "spam", "spam", "bacon", "spam"],
-]
+# menu = [
+#     ["egg", "bacon"],
+#     ["egg", "sausage", 'bacon'],
+#     ["egg", "spam"],
+#     ["egg", 'bacon', "spam"],
+#     ["egg", 'bacon', 'sausage', 'spam'],
+#     ["spam", 'bacon', 'sausage', 'spam'],
+#     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+#     ["spam", "egg", "spam", "spam", "bacon", "spam"],
+# ]
 
 # for meal in menu:
 #     for index in range(len(meal) - 1, -1, -1):
@@ -388,19 +388,28 @@ menu = [
 
 
 ### video 118 The join mathod
-menu = [
-    ["egg", "bacon"],
-    ["egg", "sausage", 'bacon'],
-    ["egg", "spam"],
-    ["egg", 'bacon', "spam"],
-    ["egg", 'bacon', 'sausage', 'spam'],
-    ["spam", 'bacon', 'sausage', 'spam'],
-    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
-    ["spam", "egg", "spam", "spam", "bacon", "spam"],
-]
+# menu = [
+#     ["egg", "bacon"],
+#     ["egg", "sausage", 'bacon'],
+#     ["egg", "spam"],
+#     ["egg", 'bacon', "spam"],
+#     ["egg", 'bacon', 'sausage', 'spam'],
+#     ["spam", 'bacon', 'sausage', 'spam'],
+#     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+#     ["spam", "egg", "spam", "spam", "bacon", "spam"],
+# ]
 
-for meal in menu:
-    for index in range(len(meal) - 1, -1, -1):
-        if(meal[index] == "spam"):
-            del meal[index]
-    print(", ".join(meal))
+# for meal in menu:
+#     for index in range(len(meal) - 1, -1, -1):
+#         if(meal[index] == "spam"):
+#             del meal[index]
+#     print(", ".join(meal))
+
+
+### Video 119 The split method
+from ntpath import join
+
+
+panagram = "The quick brown fox jumped over the lazy dog"
+words = panagram.split(" ")
+print("".join(words))
