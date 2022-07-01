55893687892db5fdfe1e457801de33a7925b27df
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-07-02 07:07:22 +0900

    till 135 video is done

--- list.py
+++ list.py
@@ -573,21 +573,30 @@
 from nested_data import albums
 
 SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
 while True:
     print("Please choose your album (invalid choice exists) ")
     for index, (title, artist, year, songs) in enumerate(albums):
         print("{}: {}".format(index + 1, title))
     
-    choice = int(input("Please enter your favourite album name: "))
+    choice = int(input("Please enter your favourite album index: "))
     if 1 <= choice <= len(albums):
         songs_list = albums[choice - 1][SONGS_LIST_INDEX]
 
     else:
         break
     
-    print(albums[choice - 1])
-    print(songs_list)
-    print()
+    print("Please Choose your song: ")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
 
-    break
+    song_choice = int(input("Please choose the song number: "))
+    if 1<= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+    else:
+        break
+
+    print("Playing {}".format(title))
+    print("*" * 30)
+
 
