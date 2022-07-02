37fe47f978427d952543f72b7484230daf718b13
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-07-03 04:34:23 +0900

    video 151 is done

--- function_video_139.py
+++ function_video_139.py
@@ -32,18 +32,44 @@
 
 ### video 145 sentence challenge solution
 
-def is_palindrom(strings):
-    return strings[::-1].casefold() == strings.casefold()
+# def is_palindrom(strings):
+#     return strings[::-1].casefold() == strings.casefold()
 
-def palindrome_sentence(sentence):
-    string = ""
-    for char in sentence:
-        if char.isalnum():
-            string += char
-    return is_palindrom(string)
+# def palindrome_sentence(sentence):
+#     string = ""
+#     for char in sentence:
+#         if char.isalnum():
+#             string += char
+#     return is_palindrom(string)
 
-sentence = input("Please enter a word to check: ")
-if palindrome_sentence(sentence):
-    print("{} is a palindrom".format(sentence))
-else:
-    print("{} is not a palindrom".format(sentence))
+# sentence = input("Please enter a word to check: ")
+# if palindrome_sentence(sentence):
+#     print("{} is a palindrom".format(sentence))
+# else:
+#     print("{} is not a palindrom".format(sentence))
+
+
+### video 151 Functions that perfom actions means it not return anything
+def banner_text(text):
+    screen_width = 80
+    if len(text) > screen_width - 4:
+        print("EED!!")
+        print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
+    
+    if text == "*":
+        print("*" * screen_width)
+    else:
+        output_string = "**{0}**".format(text.center(screen_width - 4))
+        print(output_string)
+
+banner_text("*")
+banner_text("Always look on the bright side of life...")
+banner_text("If lfe seems jolly rotten,")
+banner_text("There's something you've forgotten!")
+banner_text("And that's to laugh and smile and dance and sing,")
+banner_text(" ")
+banner_text("When you're felling in the dumps,")
+banner_text("Don't be silly chumps")
+banner_text("Just purse your lips and whistle - that's the thing!")
+banner_text("And... always look on the bright side of life...")
+banner_text("*")
