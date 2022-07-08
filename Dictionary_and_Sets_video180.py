e02dfbdb82ffca66649242bc61415ce8d396c489
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-07-08 20:56:34 +0900

    video 182 is done

--- Dictionary_and_Sets_video180.py
+++ Dictionary_and_Sets_video180.py
@@ -1,19 +1,43 @@
 ### video 181 What is a dictionary
 
+# vehicles = {
+#     'dream': 'Honda 250T',
+#     'roadster': 'BMW R1100',
+#     'er5': 'Kawasaki ER5',
+#     'can-am': 'Bombardier Can-Am 250',
+#     'virago': 'Yamaha XV250',
+#     'tenere': 'Yamaha XT650',
+#     'jimny': 'Suzuki Jimny 1.5',
+#     'fiesta': 'Ford Fiesta Ghia 1.4',
+# }
+
+# my_car = vehicles["fiesta"]
+# print(my_car)
+
+# learner = vehicles.get("er5")
+# print(learner)
+
+
+### video 182 Adding items to a dictionaryf
+
 vehicles = {
     'dream': 'Honda 250T',
     'roadster': 'BMW R1100',
     'er5': 'Kawasaki ER5',
-    'can-am': 'Bombardier Can-Am 250',
+    'can-am': 'Bombardier Can-Am 250',  
     'virago': 'Yamaha XV250',
     'tenere': 'Yamaha XT650',
     'jimny': 'Suzuki Jimny 1.5',
     'fiesta': 'Ford Fiesta Ghia 1.4',
 }
 
-my_car = vehicles["fiesta"]
-print(my_car)
+# for key in vehicles:
+#     print(key, vehicles[key], sep=", ")
 
-learner = vehicles.get("er5")
-print(learner)
+for key, value in vehicles.items():
+    print(key, value, sep=", ")
 
+vehicles["starfighter"] = "Lockheed F-104"
+vehicles["learjet"] = "Bombardier Learjet 75"
+vehicles["toy"] = "glider"
+print(vehicles)
