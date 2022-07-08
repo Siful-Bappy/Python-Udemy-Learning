ebe3e3ee3d760aff0310258484ca8dd170bdfd0b
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-07-08 23:24:30 +0900

    video 189 is done

--- Dictionary_and_Sets_video180.py
+++ Dictionary_and_Sets_video180.py
@@ -67,33 +67,69 @@
 
 ### video 184 --- Removing items from a dictionary
 
-vehicles = {
-    'dream': 'Honda 250T',
-    'roadster': 'BMW R1100',
-    'er5': 'Kawasaki ER5',
-    'can-am': 'Bombardier Can-Am 250',  
-    'virago': 'Yamaha XV250',
-    'tenere': 'Yamaha XT650',
-    'jimny': 'Suzuki Jimny 1.5',
-    'fiesta': 'Ford Fiesta Ghia 1.4',
-    "roadster": "Triumph Street Triple",
+# vehicles = {
+#     'dream': 'Honda 250T',
+#     'roadster': 'BMW R1100',
+#     'er5': 'Kawasaki ER5',
+#     'can-am': 'Bombardier Can-Am 250',  
+#     'virago': 'Yamaha XV250',
+#     'tenere': 'Yamaha XT650',
+#     'jimny': 'Suzuki Jimny 1.5',
+#     'fiesta': 'Ford Fiesta Ghia 1.4',
+#     "roadster": "Triumph Street Triple",
+# }
+
+# vehicles["starfighter"] = "Lockheed F-104"
+# vehicles["learjet"] = "Bombardier Learjet 75"
+# vehicles["toy"] = "glider"
+
+# del vehicles["toy"]
+
+# # import this 
+# # print(this)
+
+# result = vehicles.pop("f1", None)
+# result1 = vehicles.pop("f2", "You wish sell the learjet and you might afford a racing car")
+
+# plane = vehicles.pop("learjet")
+# print(plane)
+
+# bike = vehicles.pop("tenere", "not present")
+# print(bike)
+# print(result)                                                       
+
+
+### video 186 --- Using "in" with a dictionary
+### video 187 --- Dictionary menu challange solution
+### video 189 --- Adding items to a dictionary
+
+available_parts = {
+    "1": "computer",
+    "2": "monitor",
+    "3": "keyboard",
+    "4": "mouse",
+    "5": "HDMI Cable",
+    "6": "HDMIdvd drive",
 }
 
-vehicles["starfighter"] = "Lockheed F-104"
-vehicles["learjet"] = "Bombardier Learjet 75"
-vehicles["toy"] = "glider"
+current_choice = None
+computer_parts = {}
+while current_choice != "0":
+    if current_choice in available_parts:
+        choose_part = available_parts[current_choice]
+        if current_choice in computer_parts:
+            print(f"Removing: {choose_part}")
+            available_parts.pop(current_choice)
+        else:
+            print(f"adding: {choose_part}")
+            computer_parts[current_choice] = choose_part
+        print(f"Your dictionary now contains: {computer_parts})")
 
-del vehicles["toy"]
+    else:
+        print("Please Select a choice from the list")
+        for key, value in available_parts.items():
+            print(f" {key}: {value}")
+        print("0: to finish")
+    
+    current_choice = input("> ")
 
-# import this 
-# print(this)
-
-result = vehicles.pop("f1", None)
-result1 = vehicles.pop("f2", "You wish sell the learjet and you might afford a racing car")
-
-plane = vehicles.pop("learjet")
-print(plane)
-
-bike = vehicles.pop("tenere", "not present")
-print(bike)
-print(result)
